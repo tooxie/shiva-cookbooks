61259d595fdd4f6d02f060e9c18451f3cf0cdf11
Alvaro Mourino <mourino@amazon.com>
2015-07-03 12:13:46 +0200

    Added shiva

---
+++ shiva/metadata.rb
@@ -0,0 +1,9 @@
+name             'shiva'
+maintainer       'Alvaro Mouriño'
+maintainer_email 'alvaro@mourino.net'
+license          'MIT'
+description      'Installs/Configures shiva'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          '0.0.1'
+
+depends 'build-essential'
