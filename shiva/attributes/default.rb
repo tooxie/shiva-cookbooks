c8de5387e4cc37b6c139f14840a2389c07c6a64b
Alvaro Mourino <mourino@amazon.com>
2015-07-03 21:39:57 +0200

    Apache conf

--- shiva/attributes/default.rb
+++ shiva/attributes/default.rb
@@ -1,19 +1,17 @@
 override['libev']['version'] = '4.20'
 override[:apache][:logrotate][:mode] = '0644'
 
+default['shiva']['apache_conf'] = '/etc/apache2/sites-enabled/001-shiva.conf'
+default['shiva']['apache_conf_template'] = 'apache2.conf.erb'
 default['shiva']['git_path'] = '/var/git/shiva-server'
 default['shiva']['git_repo'] = 'https://github.com/tooxie/shiva-server.git'
-# TODO: Switch to apache in the main branch, but have a specific branch with
-# nginx conf.
-default['shiva']['nginx_conf'] = 'nginx.conf.erb'
-default['shiva']['shiva_conf_dir'] = '/var/conf'
-default['shiva']['shiva_conf_file'] = 'shiva.conf'
+default['shiva']['shiva_conf_file'] = 'local.py'
 default['shiva']['shiva_conf_template'] = 'shiva.conf.erb'
-default['shiva']['shiva_key'] = ''
-default['shiva']['shiva_log_access'] = '/var/log/nginx/shiva.access.log'
-default['shiva']['shiva_log_error'] = '/var/log/nginx/shiva.error.log'
+# http://docs.aws.amazon.com/opsworks/latest/userguide/workingstacks-json.html
+default['shiva']['shiva_key'] = ''  # Overwrite with custom JSON
 default['shiva']['shiva_media_dir_root'] = '/var/shiva'
 default['shiva']['shiva_media_dir_url'] = ''
 default['shiva']['shiva_port'] = '54174'
 default['shiva']['shiva_uri'] = ''
-default['shiva']['uwsgi_log'] = '/var/log/uwsgi.shiva.log'
+default['shiva']['wsgi_path'] = '/var/www/shiva.wsgi'
+default['shiva']['wsgi_path_template'] = 'shiva.wsgi.erb'
--- shiva/recipes/default.rb
+++ shiva/recipes/default.rb
@@ -28,28 +28,17 @@ Chef::Log.level = :debug
 include_recipe 'build-essential::default'
 include_recipe 'git'
 include_recipe 'libev'
-include_recipe 'nginx'
 include_recipe 'python::default'
 include_recipe 'xml'
 
 package 'libffi-dev'
+package 'libapache2-mod-wsgi'
 
 directory '/var/git' do
   owner  'root'
   group  'root'
   mode   '0755'
   action :create
-end
-
-directory node['shiva']['shiva_conf_dir'] do
-  owner  'root'
-  group  'root'
-  mode   '0755'
-  action :create
-end
-
-python_pip 'uwsgi' do
-  action :install
 end
 
 git node['shiva']['git_path'] do
@@ -59,39 +48,37 @@ git node['shiva']['git_path'] do
 end
 
 bash 'shiva_install' do
-  code "python setup.py install"
+  code 'python setup.py install'
   cwd node['shiva']['git_path']
 end
 
-template '/etc/nginx/sites-enabled/shiva.conf' do
-  source node['shiva']['nginx_conf']
-  variables(
-    :access_log => node['shiva']['shiva_log_access'],
-    :error_log => node['shiva']['shiva_log_error'],
-    :port => node['shiva']['shiva_port'],
-  )
+template node['shiva']['wsgi_path'] do
+  source node['shiva']['wsgi_path_template']
   action :create
 end
 
-template "#{node['shiva']['shiva_conf_dir']}/#{node['shiva']['shiva_conf_file']}" do
+template "#{node['shiva']['git_path']}/shiva/config/#{node['shiva']['shiva_conf_file']}" do
   source node['shiva']['shiva_conf_template']
   variables(
-    :server_uri => node['shiva']['shiva_uri'],
-    :secret_key => node['shiva']['shiva_key'],
     :media_dir_root => node['shiva']['shiva_media_dir_root'],
-    :media_dir_url => node['shiva']['shiva_media_dir_url']
+    :media_dir_url => node['shiva']['shiva_media_dir_url'],
+    :secret_key => node['shiva']['shiva_key'],
+    :server_uri => node['shiva']['shiva_uri'],
   )
   action :create
 end
 
-file node['shiva']['uwsgi_log'] do
-  mode '0744'
+template node['shiva']['wsgi_path'] do
+  source node['shiva']['wsgi_path_template']
   action :create
 end
 
-command = "uwsgi --socket /tmp/uwsgi.sock -w shiva.app:app --daemonize #{node['shiva']['uwsgi_log']}"
-
-bash 'shiva_run' do
-  code command
-  environment "SHIVA_CONFIG" => "#{node['shiva']['shiva_conf_dir']}/#{node['shiva']['shiva_conf_file']}"
+template node['shiva']['apache_conf'] do
+  source node['shiva']['apache_conf_template']
+  variables(
+    :port => node['shiva']['shiva_port'],
+    :shiva_path => node['shiva']['git_path'],
+    :wsgi_path => node['shiva']['wsgi_path'],
+  )
+  action :create
 end
