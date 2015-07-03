1adecc00edaab4df957984b60bbd4bc7bc4f6b6f
Alvaro Mourino <mourino@amazon.com>
2015-07-03 12:59:55 +0200

    ../README

--- shiva/metadata.rb
+++ shiva/metadata.rb
@@ -3,7 +3,7 @@ maintainer       'Alvaro Mouriño'
 maintainer_email 'alvaro@mourino.net'
 license          'MIT'
 description      'Installs/Configures shiva'
-long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+long_description IO.read(File.join(File.dirname(__FILE__), '../README.md'))
 version          '0.0.1'
 
 depends 'build-essential'
